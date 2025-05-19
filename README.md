e91f1cad6a3f3e51445f4d348a3c41d82b4cf522
Leonard Marschke <leonard@marschke.me>
2025-05-19 13:40:23 +0200

    feature: build Vikuja team mapper

--- README.md
+++ README.md
@@ -1,29 +1,42 @@
-# Keycloak Group Based Prefix Mapper
+# Keycloak Groups to Vikunja Teams mapper
 
-[![pipeline status](https://rechenknecht.net/giz/keycloak/group-based-prefix-mapper/badges/main/pipeline.svg)](https://rechenknecht.net/giz/keycloak/group-based-prefix-mapper/-/commits/main)
+[![pipeline status](https://rechenknecht.net/giz/keycloak/vikunja-team-mapper/badges/main/pipeline.svg)](https://rechenknecht.net/giz/keycloak/vikunja-team-mapper/-/commits/main)
 
-A Keycloak Mapper that adds a configurable prefix to attributes based on a user's groups.
+A Keycloak Mapper that maps Keycloak groups to Vikunja teams. The mapper allows configuration of specific attributes on groups to configure whether
+
+* Teams are public
+* How the teams are named
+* Which description teams have
+
+This mapper is compatible with Vikunja 0.25 onwards.
 
 ## Installation
 
-Place the [generated jar-file](https://rechenknecht.net/giz/keycloak/group-based-modification-mapper/-/jobs/artifacts/main/download?job=build-jar) into the Keycloak deployments folder.
-In the Keycloak Wildfly distribution it is located at `/opt/jboss/keycloak/standalone/deployments`, while in the Quarkus distribution it is located at `/opt/keycloak/providers`.
+Place the [generated jar-file](https://rechenknecht.net/giz/keycloak/vikunja-team-mapper/-/jobs/artifacts/main/download?job=build-jar) into the Keycloak deployments folder.
+In the Keycloak Quarkus distribution it is located at `/opt/keycloak/providers`.
 
 ## Mapper Configuration
 
 To activate the mapper, you must configure it for your clients.
 
-As an example, we configure the mapper in the following to modify the `family_name` claim by default.
-Note that this guide is for the new admin console.
+As an example, we add the mapper to the `vikunja` client.
 
-1. Go to the Admin Console under `Client Scopes`.
-1. The `family_name` claim is set by default by the `profile` Client Scope. Click `Edit` on the `profile` scope.
-1. In the `profile` details view, go to the `Mappers` tab. You should find the existing `family name` mapper there.
-1. Click on `Add mapper` and `By configuration` to add the custom mapper. Find the `Group Based Prefix OIDC Mapper` in the list.
-1. Configure the mapper as follows. With that configuration, users that are part of the group `Externe` will have the suffix `(extern)` appended to their `lastName` attribute inside the `family_name` claim.
+1. Go to the Admin Console, select your client.
+1. Go to `Client Scopes`.
+1. Click on the `dedicated` scopes entry (should be the first one).
+1. Select `Add Mapper` and `By Configuration`.
+1. Select `Vikunja team mapper`.
+1. Fill in the configuration form. Fill in (or use the default) attribute names on groups.
+1. Click `Save`.
+1. Go to a group of your choice.
+1. Select the `Attributes` tab.
+1. Fill in the key for the team name, you configured in your mapper configuration form. Choose a name as value.
+1. Click `Save`.
 
-    ![Example Group Based Mapper Configuration](docs/mapper-configuration.png)
+Now, your ID token should include the name of the group you chose.
 
-Similar to the `family_name` claim, you may add and modify any other claim.
-If you want to modify an existing claim in the token, make sure to get the `Claim Name` correct and `Property Name` so that the user's property is properly resolved.
-At best, simply copy the configuration from the existing mapper you want to overwrite.
+![Example Vikunja Team Mapper Configuration](docs/mapper-configuration.png)
+
+You may add the other keys to a group as well. However, only a group with a filled in name will be synced to Vikunja.
+
+It is safe to change the values of all Vikunja attributes on groups (even names) as they are synced via the Keycloak Group ID.
---
+++ src/main/java/berlin/giz/keycloak/mappers/vikunjateam/OIDCMapper.java
@@ -0,0 +1,188 @@
+package berlin.giz.keycloak.mappers.vikunjateam;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.jboss.logging.Logger;
+import org.keycloak.models.ClientSessionContext;
+import org.keycloak.models.GroupModel;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.ProtocolMapperModel;
+import org.keycloak.models.UserModel;
+import org.keycloak.models.UserSessionModel;
+import org.keycloak.protocol.oidc.mappers.AbstractOIDCProtocolMapper;
+import org.keycloak.protocol.oidc.mappers.OIDCAttributeMapperHelper;
+import org.keycloak.protocol.oidc.mappers.OIDCIDTokenMapper;
+import org.keycloak.provider.ProviderConfigProperty;
+import org.keycloak.representations.IDToken;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/* Vikunja team mapper
+ *
+ * See https://vikunja.io/docs/openid/ for documentation on how the team should get mapped into the tokens.
+ *
+ */
+public class OIDCMapper extends AbstractOIDCProtocolMapper implements OIDCIDTokenMapper {
+
+    static final String PROVIDER_ID = "vikunja-team-mapper";
+
+    static final String GROUP_ATTRIBUTE_NAME_TEAM_NAME_CONFIG = "group-attribute-name-team-name";
+    static final String GROUP_ATTRIBUTE_NAME_TEAM_DESCRIPTION_CONFIG = "group-attribute-name-description";
+    static final String DEFAULT_IS_PUBLIC_CONFIG = "default-is-public";
+    static final String GROUP_ATTRIBUTE_NAME_TEAM_IS_PUBLIC_CONFIG = "group-attribute-name-is-public";
+
+    static final List<ProviderConfigProperty> CONFIG_PROPERTIES;
+
+    private static final Logger LOGGER = Logger.getLogger(OIDCMapper.class);
+
+    static {
+        List<ProviderConfigProperty> configProperties = new ArrayList<>();
+        OIDCAttributeMapperHelper.addIncludeInTokensConfig(configProperties, OIDCMapper.class);
+
+        configProperties.add(new ProviderConfigProperty(
+            GROUP_ATTRIBUTE_NAME_TEAM_NAME_CONFIG,
+            "Group Attribute for Name",
+            "Attribute name on Keycloak Groups, which get mapped to Vikunja for team name. A group will only be sent to Vikunja if the attribute defined here is defined for this group.",
+            ProviderConfigProperty.STRING_TYPE,
+            "vikunjaTeamName"
+        ));
+        configProperties.add(new ProviderConfigProperty(
+            GROUP_ATTRIBUTE_NAME_TEAM_DESCRIPTION_CONFIG,
+            "Group Attribute for Description",
+            "Attribute name on Keycloak Groups, which get mapped to Vikunja for team description.",
+            ProviderConfigProperty.STRING_TYPE,
+            "vikunjaTeamDescription"
+        ));
+        configProperties.add(new ProviderConfigProperty(
+            GROUP_ATTRIBUTE_NAME_TEAM_IS_PUBLIC_CONFIG,
+            "Group Attribute for is public",
+            "Attribute name on Keycloak Groups, which get mapped to Vikunja to decide whether a team is public.",
+            ProviderConfigProperty.STRING_TYPE,
+            "vikunjaTeamIsPublic"
+        ));
+        configProperties.add(new ProviderConfigProperty(
+            DEFAULT_IS_PUBLIC_CONFIG,
+            "Teams are public by default",
+            "Configures whether a Vikunja Team will be public by default if the corresponding attribute is not set for the Keycloak group.",
+            ProviderConfigProperty.BOOLEAN_TYPE,
+            "false"
+        ));
+
+        CONFIG_PROPERTIES = configProperties;
+    }
+
+    @Override
+    public String getDisplayCategory() {
+        return TOKEN_MAPPER_CATEGORY;
+    }
+
+    @Override
+    public String getDisplayType() {
+        return "Vikunja team mapper";
+    }
+
+    @Override
+    public String getId() {
+        return PROVIDER_ID;
+    }
+
+    @Override
+    public String getHelpText() {
+        return "An OIDC mapper mapping Keycloak Groups to Vikunja teams in ID token.";
+    }
+
+    @Override
+    public List<ProviderConfigProperty> getConfigProperties() {
+        return CONFIG_PROPERTIES;
+    }
+
+    private Stream<Map<String, Object>> getVikunjaTeams(ProtocolMapperModel mappingModel, UserSessionModel userSession) {
+        String teamNameAttributeName = mappingModel.getConfig().get(GROUP_ATTRIBUTE_NAME_TEAM_NAME_CONFIG);
+        String teamDescriptionAttributeName = mappingModel.getConfig().get(GROUP_ATTRIBUTE_NAME_TEAM_DESCRIPTION_CONFIG);
+        String defaultTeamIsPublicString = mappingModel.getConfig().get(DEFAULT_IS_PUBLIC_CONFIG);
+        String teamIsPublicAttributeName = mappingModel.getConfig().get(GROUP_ATTRIBUTE_NAME_TEAM_IS_PUBLIC_CONFIG);
+
+        // Ensure all required settings are properly set
+        if (teamNameAttributeName == null || defaultTeamIsPublicString == null) {
+            LOGGER.debug("Some Vikunja team mapper settings are not properly set");
+            return null;
+        }
+
+        Boolean defaultIsPublic = Boolean.valueOf(defaultTeamIsPublicString);
+
+        UserModel user = userSession.getUser();
+
+        // Unfortunately, there is no easy possibility to retrieve all groups a user is member of.
+        // However, we would like to resemble the role hierarchy behavior built into Keycloak.
+        // Keycloak's UserModel.getGroupsStream() only returns direct group memberships,
+        // so we need to manually traverse the hierarchy to include parent groups.
+        Map<String, GroupModel> teamsUserIsMemberOf = new HashMap<>();
+        user.getGroupsStream().forEach(currentUserGroup -> {
+            GroupModel currentGroup = currentUserGroup;
+            while (currentGroup != null && !teamsUserIsMemberOf.containsKey(currentGroup.getId())) {
+                teamsUserIsMemberOf.put(currentGroup.getId(), currentGroup);
+                currentGroup = currentGroup.getParent();
+            }
+        });
+
+        Stream<GroupModel> groupStream = teamsUserIsMemberOf.values().stream();
+        return groupStream.map(group -> {
+            Map<String, Object> res = new HashMap<>();
+
+            String teamName = group.getFirstAttribute(teamNameAttributeName);
+            if (teamName == null) {
+                // do not add Keycloak group to Vikunja if no name is explicitly set
+                return null;
+            }
+            res.put("oidcID", group.getId());
+            res.put("name", teamName);
+
+            if (teamIsPublicAttributeName != null) {
+                String isPublicString = group.getFirstAttribute(teamIsPublicAttributeName);
+                Boolean isPublic = isPublicString == null ? defaultIsPublic : Boolean.valueOf(isPublicString);
+                res.put("isPublic", isPublic);
+            } else {
+                res.put("isPublic", defaultIsPublic);
+            }
+
+            if (teamDescriptionAttributeName != null) {
+                String teamDescription = group.getFirstAttribute(teamDescriptionAttributeName);
+                if (teamDescription != null) {
+                    res.put("description", teamDescription);
+                }
+            }
+
+            return res;
+        }).filter(Objects::nonNull);
+    }
+
+    @Override
+    protected void setClaim(IDToken token, ProtocolMapperModel mappingModel, UserSessionModel userSession,
+        KeycloakSession keycloakSession, ClientSessionContext clientSessionCtx
+    ) {
+        Map<String, String> mappingConfig = mappingModel.getConfig();
+        // This is a hardcoded claim as vikunja does not support other claim names at the time of writing this.
+        // If this changes, feel free to change / open an issue.
+        mappingConfig.put(OIDCAttributeMapperHelper.TOKEN_CLAIM_NAME, "vikunja_groups");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<Map<String, Object>> teams = this.getVikunjaTeams(mappingModel, userSession).collect(Collectors.toList());
+        JsonNode claimNode = objectMapper.valueToTree(teams);
+
+        // Map claim into token
+        OIDCAttributeMapperHelper.mapClaim(token, mappingModel, claimNode);
+    }
+
+    @Override
+    public int getPriority() {
+        // Use default priority to allow manual overwriting
+        return 0;
+    }
+}
